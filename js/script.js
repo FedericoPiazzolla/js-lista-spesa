693f4d0c3212a82ff2c43d6ab95c4a5bac894737
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-25 18:02:35 +0200

    add: element by html, my-list. and print on html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const shoppingList = ["latte", "uova", "pane", "caffe", "prosciutto"];
 console.log(shoppingList);
 
+const myList = document.getElementById("my-list");
+
 let i = 0;
 let listString = "";
 
 while (i < shoppingList.length) {
   console.log(i);
-  i++;
 
-  const curItem = shoppingList[i];
-  listString += `<li>${curItem}</li>`;
-  console.log(listString)
+  listString = `<li>${shoppingList[i]}</li>`;
+  myList.innerHTML += listString
+  i++;
+  console.log(listString);
 }
